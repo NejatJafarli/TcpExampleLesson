c37013fdbbd60f7cc1dec7f303359fdd93ceb4e1
nicat ceferli <mega.ceferli@gmail.com>
2022-01-28 23:29:38 +0400

    Add project files.

---
+++ Tcp_Client/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+//tcp client
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+        var ip = IPAddress.Loopback;
+        var port = 11111;
+
+        Console.WriteLine("Client");
+        var client = new TcpClient();
+        client.Connect(ip, port);
+        BinaryReader br = new BinaryReader(client.GetStream());
+        BinaryWriter bw = new BinaryWriter(client.GetStream());
+
+
+        while (true)
+        {
+            var str = Console.ReadLine().ToUpper();
+            if (str == "HELP")
+            {
+                Console.WriteLine(Commands.Proclist);
+                Console.WriteLine(Commands.Kill, " <Process_Name>");
+                Console.WriteLine(Commands.Run, " <Process_Name>");
+                Console.WriteLine("HELP");
+                continue;
+            }
+
+            Commands command = new Commands();
+
+            var TextAndParam = str.Split(' ');
+            string responce = null;
+            switch (TextAndParam[0])
+            {
+                case Commands.Proclist:
+                    command.Text = TextAndParam[0];
+
+                    bw.Write(JsonSerializer.Serialize(command));
+
+                    responce = br.ReadString();
+                    var Process = JsonSerializer.Deserialize<string[]>(responce);
+
+                    foreach (var item in Process)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                    break;
+                case Commands.Run:
+
+                    command.Text = TextAndParam[0];
+                    command.Param = TextAndParam[1];
+
+                    bw.Write(JsonSerializer.Serialize(command));
+                    if (br.ReadBoolean())
+                        Console.WriteLine($"Runned --> {command.Param}");
+                    break;
+                case Commands.Kill:
+
+                    command.Text = TextAndParam[0];
+                    command.Param = TextAndParam[1];
+                    bw.Write(JsonSerializer.Serialize(command));
+                    if (br.ReadBoolean())
+                        Console.WriteLine($"Killed --> {command.Param}");
+                    
+                    break;
+            }
+
+        }
+
+
+    }
+
+}
+
+class Commands
+{
+    public const string Proclist = "PROCLIST";
+    public const string Kill = "KILL";
+    public const string Run = "RUN";
+    public string Text { get; set; }
+    public string Param { get; set; }
+}
---
+++ Tcp_Server/Program.cs
@@ -0,0 +1,93 @@
+﻿
+//TCP Client
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var ip = IPAddress.Loopback;
+        var port = 11111;
+
+        var Server = new TcpListener(ip, port);
+        Server.Start(100);
+
+        while (true)
+        {
+            var client = Server.AcceptTcpClient();
+
+            var stream = client.GetStream();
+
+            var br = new BinaryReader(stream);
+            var bw = new BinaryWriter(stream);
+
+            Console.WriteLine("Server");
+            while (true)
+            {
+                var input = br.ReadString();
+                if (input == "Exit")
+                    break;
+                var Command = JsonSerializer.Deserialize<Commands>(input);
+                if (Command == null) continue;
+
+                Console.WriteLine(Command.Text);
+                Console.WriteLine(Command.Param);
+
+                switch (Command.Text)
+                {
+                    case Commands.Proclist:
+                        var Proces = Process.GetProcesses();
+                        var JsonText = JsonSerializer.Serialize(Proces.Select(p => p.ProcessName));
+                        bw.Write(JsonText);
+                        break;
+                    case Commands.Run:
+
+                        try
+                        {
+                            var Run = Command.Param;
+                            Process.Start(Run);
+                            bw.Write("1");
+                        }
+                        catch (Exception ex)
+                        {
+
+
+                            bw.Write("0");
+                            throw;
+                        }
+
+                        break;
+                    case Commands.Kill:
+                        try
+                        {
+                            var kill = Process.GetProcessesByName(Command.Param).FirstOrDefault();
+                            kill?.Kill();
+                            bw.Write("1");
+                        }
+                        catch (Exception ex)
+                        {
+                            bw.Write("0");
+                        }
+                        break;
+                }
+
+            }
+
+
+        }
+
+
+    }
+}
+
+class Commands
+{
+    public const string Proclist = "PROCLIST";
+    public const string Kill = "KILL";
+    public const string Run = "RUN";
+    public string Text { get; set; }
+    public string Param { get; set; }
+}
